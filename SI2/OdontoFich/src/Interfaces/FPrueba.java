56a734219b7ef1a1241c2ef4781d25f3f3b88e5e
Julio7-7-7 <julio.toledo2030@gmail.com>
2024-12-14 01:55:26 -0400

    estilizando botones

--- SI2/OdontoFich/src/Interfaces/FPrueba.java
+++ SI2/OdontoFich/src/Interfaces/FPrueba.java
@@ -1,24 +1,29 @@
 package Interfaces;
- 
 import clases.CPrueba;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+import javax.swing.JComponent;
 
 public class FPrueba extends javax.swing.JFrame {
+     public FPrueba() {
+     initComponents();
+     setLocationRelativeTo(null);
+     aplicarEfectoBotones();
+}
  
-    public FPrueba() {
-        initComponents();
-        setLocationRelativeTo(null);
-        
-        
-    }
- 
-    @SuppressWarnings("unchecked")
+@SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-        txtregistro = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
@@ -26,60 +31,79 @@ public class FPrueba extends javax.swing.JFrame {
         txtgestion = new javax.swing.JTextField();
         panelmaterias = new javax.swing.JPanel();
         jSeparator1 = new javax.swing.JSeparator();
-        jSeparator2 = new javax.swing.JSeparator();
         jSeparator3 = new javax.swing.JSeparator();
-        TOTAL = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        txtregistro = new javax.swing.JTextField();
+        jSeparator2 = new javax.swing.JSeparator();
         BBuscar = new javax.swing.JButton();
+        TOTAL = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel1.setFont(new java.awt.Font("Trebuchet MS", 0, 24)); // NOI18N
-        jLabel1.setText("TRABAJOS");
-        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 10, 120, 30));
-
-        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jLabel2.setText("Registro:");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 50, -1, -1));
-
-        txtregistro.setBorder(null);
-        jPanel1.add(txtregistro, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 50, 170, 20));
-
+        jLabel3.setBackground(new java.awt.Color(0, 0, 0));
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         jLabel3.setText("Estudiante:");
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 130, -1, -1));
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 120, 70, -1));
 
-        jLabel4.setText("Gestión");
-        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 170, -1, -1));
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel4.setText("Gestión:");
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 150, 60, 30));
 
         jLabel5.setFont(new java.awt.Font("MS Reference Sans Serif", 0, 18)); // NOI18N
         jLabel5.setText("MATERIAS");
-        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 200, -1, -1));
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 180, -1, -1));
 
+        txtestudiante.setEditable(false);
+        txtestudiante.setBackground(new java.awt.Color(255, 255, 255));
+        txtestudiante.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         txtestudiante.setBorder(null);
-        jPanel1.add(txtestudiante, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 130, 240, -1));
+        jPanel1.add(txtestudiante, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 120, 240, -1));
 
+        txtgestion.setEditable(false);
+        txtgestion.setBackground(new java.awt.Color(255, 255, 255));
+        txtgestion.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         txtgestion.setBorder(null);
-        jPanel1.add(txtgestion, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 170, 200, 20));
+        jPanel1.add(txtgestion, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 150, 200, 20));
 
         panelmaterias.setBackground(new java.awt.Color(255, 255, 255));
         panelmaterias.setOpaque(false);
         panelmaterias.setLayout(new javax.swing.BoxLayout(panelmaterias, javax.swing.BoxLayout.LINE_AXIS));
-        jPanel1.add(panelmaterias, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 250, 320, 340));
-        jPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 190, 200, 10));
-        jPanel1.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 70, 170, 10));
-        jPanel1.add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 150, 240, 10));
-
-        TOTAL.setText("TOTAL RECAUDADO");
-        TOTAL.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                TOTALActionPerformed(evt);
-            }
-        });
-        jPanel1.add(TOTAL, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 80, -1, -1));
+        jPanel1.add(panelmaterias, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 210, 220, 380));
+        jPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 170, 40, 10));
+        jPanel1.add(jSeparator3, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 140, 240, 10));
 
         jPanel2.setBackground(new java.awt.Color(0, 153, 255));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Trebuchet MS", 3, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(230, 230, 230));
+        jLabel1.setText("TRABAJOS");
+        jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 20, 120, 30));
+
+        jLabel2.setBackground(new java.awt.Color(214, 214, 214));
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Registro:");
+        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 10, -1, 40));
+
+        txtregistro.setBackground(new java.awt.Color(0, 153, 255));
+        txtregistro.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        txtregistro.setForeground(new java.awt.Color(255, 255, 255));
+        txtregistro.setBorder(null);
+        txtregistro.setDisabledTextColor(new java.awt.Color(255, 255, 255));
+        txtregistro.setOpaque(true);
+        txtregistro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtregistroActionPerformed(evt);
+            }
+        });
+        jPanel2.add(txtregistro, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 20, 170, 20));
+        jPanel2.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 40, 70, 10));
 
         BBuscar.setText("BUSCAR");
         BBuscar.addActionListener(new java.awt.event.ActionListener() {
@@ -87,25 +111,17 @@ public class FPrueba extends javax.swing.JFrame {
                 BBuscarActionPerformed(evt);
             }
         });
+        jPanel2.add(BBuscar, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 60, -1, -1));
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(351, Short.MAX_VALUE)
-                .addComponent(BBuscar)
-                .addGap(31, 31, 31))
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(47, Short.MAX_VALUE)
-                .addComponent(BBuscar)
-                .addGap(45, 45, 45))
-        );
+        TOTAL.setText("TOTAL RECAUDADO");
+        TOTAL.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TOTALActionPerformed(evt);
+            }
+        });
+        jPanel2.add(TOTAL, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 60, -1, -1));
 
-        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 460, -1));
+        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 460, 110));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -121,22 +137,6 @@ public class FPrueba extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void BBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BBuscarActionPerformed
-        CPrueba objetoprueba = new CPrueba();
-        objetoprueba.BuscarYLlenar(txtregistro, txtestudiante, txtgestion, panelmaterias);
-    }//GEN-LAST:event_BBuscarActionPerformed
-
-    private void TOTALActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TOTALActionPerformed
-
-  setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-            FTotalRecaudado Total = new FTotalRecaudado();
-            Total.setVisible(true);
-            setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-
-            //this.dispose();
-
-    }//GEN-LAST:event_TOTALActionPerformed
- 
     public static void main(String args[]) {        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -144,6 +144,148 @@ public class FPrueba extends javax.swing.JFrame {
             }
         });
     }
+    
+    private void BBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BBuscarActionPerformed
+        CPrueba objetoprueba = new CPrueba();
+        objetoprueba.BuscarYLlenar(txtregistro, txtestudiante, txtgestion, panelmaterias);
+    }//GEN-LAST:event_BBuscarActionPerformed
+
+    private void TOTALActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TOTALActionPerformed
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        FTotalRecaudado Total = new FTotalRecaudado();
+        Total.setVisible(true);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    }//GEN-LAST:event_TOTALActionPerformed
+
+    private void txtregistroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtregistroActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtregistroActionPerformed
+    
+    private void aplicarEfectoBotones() {
+        efectoBotones(BBuscar);
+        efectoBotones(TOTAL);
+    }
+
+    private void efectoBotones(JButton button) {
+    // Asegurar transparencia total
+    button.setOpaque(false);
+    button.setContentAreaFilled(false);
+    button.setFocusPainted(false);
+
+    // Personalizar texto
+    button.setForeground(Color.WHITE);
+    button.setFont(new Font("Arial", Font.BOLD, 14));
+
+    // Crear un borde redondeado visible
+    button.setBorder(new javax.swing.border.AbstractBorder() {
+        @Override
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            // Color y grosor del borde
+            g2d.setColor(new Color(200, 200, 200)); // Gris claro
+            g2d.setStroke(new java.awt.BasicStroke(2)); // Grosor del borde
+
+            // Dibujar el borde redondeado
+            g2d.drawRoundRect(x, y, width - 1, height - 1, 30, 30); // Radio de 30 para bordes redondeados
+        }
+
+        @Override
+        public Insets getBorderInsets(Component c) {
+            return new Insets(7, 7, 7, 7); // Ajuste interno entre texto y borde
+        }
+
+        @Override
+        public boolean isBorderOpaque() {
+            return false; // Garantiza que el fondo sea transparente
+        }
+    });
+
+    // Cambiar cursor al pasar sobre el botón
+    button.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+
+    // Agregar efectos visuales al pasar el mouse
+    button.addMouseListener(new MouseAdapter() {
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            // Obtener el color del texto actual
+            Color textColor = button.getForeground();
+
+            // Cambiar el color del texto y del borde al pasar el mouse
+            button.setForeground(new Color(202, 207, 250)); // Blanco con transparencia ajustada
+            button.repaint();
+
+            // Cambiar el color del borde para que coincida con el color del texto
+            button.setBorder(new javax.swing.border.AbstractBorder() {
+                @Override
+                public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+                    Graphics2D g2d = (Graphics2D) g;
+                    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+                    // Usar el mismo color que el texto para el borde
+                    g2d.setColor(button.getForeground()); // Usar el color de texto
+                    g2d.setStroke(new java.awt.BasicStroke(2)); // Grosor del borde
+
+                    // Dibujar el borde redondeado
+                    g2d.drawRoundRect(x, y, width - 1, height - 1, 30, 30); // Radio de 30 para bordes redondeados
+                }
+
+                @Override
+                public Insets getBorderInsets(Component c) {
+                    return new Insets(10, 10, 10, 10); // Ajuste interno entre texto y borde
+                }
+
+                @Override
+                public boolean isBorderOpaque() {
+                    return false; // Garantiza que el fondo sea transparente
+                }
+            });
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            // Revertir el texto y el borde a sus valores originales
+            button.setForeground(Color.WHITE); // Revertir el texto a blanco
+            button.repaint();
+
+            // Revertir el borde al color original
+            button.setBorder(new javax.swing.border.AbstractBorder() {
+                @Override
+                public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+                    Graphics2D g2d = (Graphics2D) g;
+                    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+                    // Color y grosor del borde original
+                    g2d.setColor(new Color(200, 200, 200)); // Gris claro
+                    g2d.setStroke(new java.awt.BasicStroke(2)); // Grosor del borde
+
+                    // Dibujar el borde redondeado
+                    g2d.drawRoundRect(x, y, width - 1, height - 1, 30, 30); // Radio de 30 para bordes redondeados
+                }
+
+                @Override
+                public Insets getBorderInsets(Component c) {
+                    return new Insets(7, 7, 7, 7); // Ajuste interno entre texto y borde
+                }
+
+                @Override
+                public boolean isBorderOpaque() {
+                    return false; // Garantiza que el fondo sea transparente
+                }
+            });
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton BBuscar;
--- SI2/OdontoFich/src/Interfaces/FPruebaDetalles.java
+++ SI2/OdontoFich/src/Interfaces/FPruebaDetalles.java
@@ -1,13 +1,27 @@
 package Interfaces;
  
 import clases.CPruebaDetalle;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class FPruebaDetalles extends javax.swing.JFrame {
  
     public FPruebaDetalles() {
         initComponents();
+        aplicarEfectoBotones();
     }
+    private CPruebaDetalle pruebaDetalle = new CPruebaDetalle();
+
  
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -21,7 +35,9 @@ public class FPruebaDetalles extends javax.swing.JFrame {
         txtnombremateria = new javax.swing.JTextField();
         txtestudiante = new javax.swing.JTextField();
         paneldetalle = new javax.swing.JPanel();
-        BMostrar = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        txtregistro = new javax.swing.JTextField();
+        binforme = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -57,21 +73,25 @@ public class FPruebaDetalles extends javax.swing.JFrame {
         paneldetalle.setBackground(new java.awt.Color(153, 153, 153));
         paneldetalle.setOpaque(false);
         paneldetalle.setLayout(new javax.swing.BoxLayout(paneldetalle, javax.swing.BoxLayout.LINE_AXIS));
-        jPanel1.add(paneldetalle, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 140, 600, 440));
+        jPanel1.add(paneldetalle, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 140, 800, 440));
 
-        BMostrar.setText("TRABAJOS");
-        BMostrar.addActionListener(new java.awt.event.ActionListener() {
+        jLabel2.setText("Registro:");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 120, -1, -1));
+        jPanel1.add(txtregistro, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 110, 150, 30));
+
+        binforme.setText("INFORME");
+        binforme.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                BMostrarActionPerformed(evt);
+                binformeActionPerformed(evt);
             }
         });
-        jPanel1.add(BMostrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 60, -1, -1));
+        jPanel1.add(binforme, new org.netbeans.lib.awtextra.AbsoluteConstraints(520, 60, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 724, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 852, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -85,27 +105,24 @@ public class FPruebaDetalles extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_txtsiglamateriaActionPerformed
 
-    private void BMostrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BMostrarActionPerformed
-        CPruebaDetalle objetoprueba = new CPruebaDetalle();
-        String siglaMateria = txtsiglamateria.getText(); // Obtener la sigla de la materia desde el campo de texto
-    if (siglaMateria.isEmpty()) {
-        JOptionPane.showMessageDialog(null, "Por favor, ingresa una sigla de materia válida.");
-        return;
-    }
-
-    objetoprueba.mostrarTrabajos(siglaMateria, paneldetalle); // Llama al método y pasa el panel donde se mostrarán los trabajos
-    ;
-    }//GEN-LAST:event_BMostrarActionPerformed
-
     private void txtestudianteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtestudianteActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_txtestudianteActionPerformed
+
+    private void binformeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_binformeActionPerformed
+         String siglaMateria = txtsiglamateria.getText(); // Suponiendo que tienes un JTextField llamado txtSiglaMateria
+
+    // Llamar al método mostrarTrabajosYPagos desde la instancia
+    pruebaDetalle.mostrarTrabajosYPagos(siglaMateria, paneldetalle);
+    }//GEN-LAST:event_binformeActionPerformed
  
-    public void setDatos(String sigla, String nombre, String estudiante) {
+   public void setDatos(String sigla, String nombre, String estudiante, String registro) {
     txtsiglamateria.setText(sigla);
     txtnombremateria.setText(nombre);
     txtestudiante.setText(estudiante);
-    }
+    txtregistro.setText(registro); // Actualizar el JTextField con el registro
+}
+
 
     public static void main(String args[]) {
  
@@ -115,16 +132,134 @@ public class FPruebaDetalles extends javax.swing.JFrame {
             }
         });
     }
+    
+    private void aplicarEfectoBotones() {
+        efectoBotones(binforme);
+    }
+
+    private void efectoBotones(JButton button) {
+    // Asegurar transparencia total
+    button.setOpaque(false);
+    button.setContentAreaFilled(false);
+    button.setFocusPainted(false);
+
+    // Personalizar texto
+    button.setForeground(Color.WHITE);
+    button.setFont(new Font("Arial", Font.BOLD, 14));
+
+    // Crear un borde redondeado visible
+    button.setBorder(new javax.swing.border.AbstractBorder() {
+        @Override
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            // Color y grosor del borde
+            g2d.setColor(new Color(200, 200, 200)); // Gris claro
+            g2d.setStroke(new java.awt.BasicStroke(2)); // Grosor del borde
+
+            // Dibujar el borde redondeado
+            g2d.drawRoundRect(x, y, width - 1, height - 1, 30, 30); // Radio de 30 para bordes redondeados
+        }
+
+        @Override
+        public Insets getBorderInsets(Component c) {
+            return new Insets(7, 7, 7, 7); // Ajuste interno entre texto y borde
+        }
+
+        @Override
+        public boolean isBorderOpaque() {
+            return false; // Garantiza que el fondo sea transparente
+        }
+    });
+
+    // Cambiar cursor al pasar sobre el botón
+    button.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+
+    // Agregar efectos visuales al pasar el mouse
+    button.addMouseListener(new MouseAdapter() {
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            // Obtener el color del texto actual
+            Color textColor = button.getForeground();
+
+            // Cambiar el color del texto y del borde al pasar el mouse
+            button.setForeground(new Color(202, 207, 250)); // Blanco con transparencia ajustada
+            button.repaint();
+
+            // Cambiar el color del borde para que coincida con el color del texto
+            button.setBorder(new javax.swing.border.AbstractBorder() {
+                @Override
+                public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+                    Graphics2D g2d = (Graphics2D) g;
+                    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+                    // Usar el mismo color que el texto para el borde
+                    g2d.setColor(button.getForeground()); // Usar el color de texto
+                    g2d.setStroke(new java.awt.BasicStroke(2)); // Grosor del borde
+
+                    // Dibujar el borde redondeado
+                    g2d.drawRoundRect(x, y, width - 1, height - 1, 30, 30); // Radio de 30 para bordes redondeados
+                }
+
+                @Override
+                public Insets getBorderInsets(Component c) {
+                    return new Insets(10, 10, 10, 10); // Ajuste interno entre texto y borde
+                }
+
+                @Override
+                public boolean isBorderOpaque() {
+                    return false; // Garantiza que el fondo sea transparente
+                }
+            });
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            // Revertir el texto y el borde a sus valores originales
+            button.setForeground(Color.WHITE); // Revertir el texto a blanco
+            button.repaint();
+
+            // Revertir el borde al color original
+            button.setBorder(new javax.swing.border.AbstractBorder() {
+                @Override
+                public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+                    Graphics2D g2d = (Graphics2D) g;
+                    g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+                    // Color y grosor del borde original
+                    g2d.setColor(new Color(200, 200, 200)); // Gris claro
+                    g2d.setStroke(new java.awt.BasicStroke(2)); // Grosor del borde
+
+                    // Dibujar el borde redondeado
+                    g2d.drawRoundRect(x, y, width - 1, height - 1, 30, 30); // Radio de 30 para bordes redondeados
+                }
+
+                @Override
+                public Insets getBorderInsets(Component c) {
+                    return new Insets(7, 7, 7, 7); // Ajuste interno entre texto y borde
+                }
+
+                @Override
+                public boolean isBorderOpaque() {
+                    return false; // Garantiza que el fondo sea transparente
+                }
+            });
+        }
+    });
+}
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton BMostrar;
+    private javax.swing.JButton binforme;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel paneldetalle;
     private javax.swing.JTextField txtestudiante;
     private javax.swing.JTextField txtnombremateria;
+    private javax.swing.JTextField txtregistro;
     private javax.swing.JTextField txtsiglamateria;
     // End of variables declaration//GEN-END:variables
 }
